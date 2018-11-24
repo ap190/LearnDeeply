4015c281380179aadcdcf4350987457b25b072e6
ap190 <Alexandra_Paul@brown.edu>
2018-11-24 15:58:57 -0500

    more data

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# A script to collect lots of data.
+python instagram-crawler/crawler.py  -u kyliejenner
+python instagram-crawler/crawler.py  -u kimkardashian
+python instagram-crawler/crawler.py -u voyaged
+python instagram-crawler/crawler.py -u jakepaul
+python instagram-crawler/crawler.py -u camerondallas
+python instagram-crawler/crawler.py -u kendalljenner
+python instagram-crawler/crawler.py -u emrata
+python instagram-crawler/crawler.py -u brightbazaar
+python instagram-crawler/crawler.py -u alexachung
