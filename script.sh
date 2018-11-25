7692ea3bcd8d34533e86df1e4dd465599a1d390e
ap190 <Alexandra_Paul@brown.edu>
2018-11-25 00:15:57 -0500

    100 more entries

--- script.sh
+++ script.sh
@@ -2,6 +2,5 @@
 
 # A script to collect lots of data.
 python instagram-crawler/crawler.py -u selenagomez
-python instagram-crawler/crawler.py -u cristiano
 python instagram-crawler/crawler.py -u taylorswift
 python instagram-crawler/crawler.py -u leomessi
