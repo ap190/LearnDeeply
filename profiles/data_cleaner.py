8ff80cdbf9bb37af549d24a69710ae5411c1aac1
Austin Tung <austin_tung@brown.edu>
2018-11-27 17:41:08 -0500

    data cleaner with date in readable format and image compressed into 2d array ready for keras models

--- profiles/data_cleaner.py
+++ profiles/data_cleaner.py
@@ -1,6 +1,8 @@
 import cv2
+import dateutil.parser as dateparser
 import numpy as np
 import os
+from PIL import Image
 import json
 import urllib
 
@@ -21,46 +23,63 @@ filenames = [file for file in os.listdir() if file.endswith('.json')]
 # each entry in the list will be a dictionary for each insta user we've collected
 data = []
 for filename in filenames:
-    with open(filename) as file:
-        # load json file
-        jsonfile = json.load(file)
+    try:
+        with open(filename) as file:
+            # load json file
+            jsonfile = json.load(file)
 
-        # extact user specific data
-        user_data = {
-            'user'     : jsonfile['alias'],
-            'posts'    : jsonfile['numberPosts'],
-            'followers': jsonfile['numberFollowers'],
-            'following': jsonfile['numberFollowing'],
-            'images'   : []
-        }
+            # extact user specific data
+            user_data = {
+                'user'     : jsonfile['alias'],
+                'posts'    : jsonfile['numberPosts'],
+                'followers': jsonfile['numberFollowers'],
+                'following': jsonfile['numberFollowing'],
+                'images'   : []
+            }
 
-        # extract and compress images
-        for image in jsonfile['posts']:
-            if not image['isVideo']:
-                res = urllib.request.urlopen(image['urlImage'])
-                img = np.asarray(bytearray(res.read()), dtype='uint8')
+            # extract and compress images from the user's instagram page
+            for image in jsonfile['posts']:
+                if not image['isVideo']:
+                    try: 
+                        # open image from URL and obtain compressed array representation for keras models
+                        res = urllib.request.urlopen(image['urlImage'])
+                        img = img_to_array(load_img(res, target_size=in_shape))
+                        img = np.expand_dims(img_to_array(img), axis=0)
+                        processed_img = preprocess_input(img)
 
-                img = cv2.imdecode(img, cv2.IMREAD_COLOR)
-                img = load_img(img, target_size=in_shape)
-                img = np.expand_dims(img_to_array(img), axis=0)
-                processed_img = preprocess_input(img)
+                        # conduct image classification with Inception ResNet model
+                        predictions = model.predict(processed_img)
+                        predictions = imagenet_utils.decode_predictions(predictions)
+                        predictions = np.delete(np.asarray(predictions[0]), np.s_[0], axis=1)
 
-                print(processed_img)
-                stop
+                        # preprocess and restructure date information into more readable format
+                        date = dateparser.parse(image['date'])
 
-                image_data = {
-                    'picture'    : processed_img,
-                    'tags'       : image['tags'],
-                    'mentions'   : image['mentions'],
-                    'description': image['description'],
-                    'date'       : image['date'],
-                    'likes'      : image['numberLikes']
-                }
+                        # store metadata into dictionary
+                        image_data = {
+                            'picture'       : processed_img,
+                            'classification': predictions,
+                            'tags'          : image['tags'],
+                            'mentions'      : image['mentions'],
+                            'description'   : image['description'],
+                            'weekday'       : date.weekday(),
+                            'hour'          : date.hour,
+                            'likes'         : image['numberLikes']
+                        }
 
-                user_data['images'].append(image_data)
+                        # append image data dictionary into the instagram user's image posts
+                        user_data['images'].append(image_data)
+                    except:
+                        continue
 
-        data.append(user_data)
-    file.close()
+            # append instagram user's data and list of image posts into overall list
+            data.append(user_data)
 
+        # close open file in preparation for next iteration
+        file.close()
+    except: 
+        continue
+
+# write a new data.json file with the produced dictionary object
 with open('../data.json', 'w') as outfile:
     json.dump(data, outfile, ensure_ascii=False, indent=2)
