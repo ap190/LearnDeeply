a3bbdca8ef00ba85d1fa3183742e8dc9fe9e8ea3
ap190 <Alexandra_Paul@brown.edu>
2018-11-29 15:10:33 -0500

    all data

---
+++ instagram_crawler/crawl.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# A script to collect lots of data.
+
+python crawl_profile.py chrisozer raldo sheidlina secdus levijfoster cbquality xenia brooklynandbailey wilde_oates therealfouz disney_nuts cesinha thefella 
+
+
+python crawl_profile.py xserrano9 streetsfashions pimtha sherifartisan ulyana.aster hairgod_zito nahcardoso thesheerconnection erubes1 kirstenzellers
+python crawl_profile.py brianskerry mariale doentesporfutebol nouraridaofficial cozinhadalbo dani_thorne christianziegler jn irablockphoto cafajestando 
+
+
+python crawl_profile.py thomaspeschak mangorabbitrabbit salvarezphoto melissafarlow lilithmoonlife realdiddykong benakribo kinoyoga jujujuxiaowen 
+
+python crawl_profile.py misscoolpics stylebook_ afnan_albatel devourpower andreamelchiorre1 mensfashions gabyespino cintascotch goldenretrievers_ vaughngittinjr
+
+python crawl_profile.py  tavicastro lyzabeth_lopez leagueofupdates instagrafite thisisamans.world aaronsanimals jessiekass joannakuchta archixxi 
+
+
+python crawl_profile.py man__magazine yoga_girl gui_ tommycaldwell cyarine laurenepbath chuck noel.alva luckybsmith eddypinto_ timethai martabevacqua colerise 
+
+
+python crawl_profile.py adenorah sassychris1 chloeszep changmakeup rayzanicacio stationcdrkelly ashybines mayonose robinmartinyoga caro_e_ kickposters ohhcouture
+python crawl_profile.py designdecor menwithstreetstyle fit_trio moonmountainman stephsinclairpix beverlyjoubert arqsketch audiosoup emilyskyefit 
+python crawl_profile.py bossbabe.inc manny_the_frenchie jasoncharleshill withlovekarissa nihatkayin loveandlemons alexandrabring instasurreal kessara livemontreal charlestrippy flettemamma adriancmurray luanasilva 
