9107a997ca7045b136f5cd6b6dd51c2b4badfac9
ap190 <Alexandra_Paul@brown.edu>
2018-12-07 15:49:26 -0500

    some more stability

--- Models/main.py
+++ Models/main.py
@@ -34,7 +34,7 @@ def imageclass():
 
     probabilities, vocab_size = data.image_class['probabilities'], data.image_class['vocab_size']
     imageNN = imageclass_NN.Model(inputs=inputs, labels=labels, probabilities=probabilities, vocab_size=vocab_size,
-        learning_rate=0.001, embed_size=100, dropout=0.25, hidden_layers=3, hidden_sizes=[100, 150, 100])
+        learning_rate=0.001, embed_size=100, dropout=0.15, hidden_layers=3, hidden_sizes=[100, 150, 100])
     imageNN.train_model(verbose=1)
     imageNN.test_model(verbose=1)
 
@@ -44,21 +44,21 @@ def imageclass():
 def main():
     inputs, labels = data.metadata['inputs'], data.metadata['labels']
     metaNN = metadata_NN.Model(inputs=inputs, labels=labels, 
-        learning_rate=0.001, dropout=0.25, hidden_layers=3, hidden_sizes=[100, 150, 100])
+        learning_rate=0.001, dropout=0, hidden_layers=3, hidden_sizes=[100, 150, 100])
 
     meta_output = metaNN.predict(inputs)
 
     inputs, labels = data.image_class['inputs'], data.image_class['labels']
     probabilities, vocab_size = data.image_class['probabilities'], data.image_class['vocab_size']
     imageNN = imageclass_NN.Model(inputs=inputs, labels=labels, probabilities=probabilities, vocab_size=vocab_size,
-        learning_rate=0.001, embed_size=100, dropout=0.25, hidden_layers=3, hidden_sizes=[100, 150, 100])
+        learning_rate=0.001, embed_size=50, dropout=0, hidden_layers=3, hidden_sizes=[100, 150, 100])
 
     image_output = imageNN.predict(inputs, probabilities)
 
     combined_inputs = np.concatenate((meta_output, image_output), axis=1)
     combined_labels = labels
     combinedNN = combined_NN.Model(inputs=combined_inputs, labels=combined_labels, 
-        learning_rate=0.001, dropout=0.25, hidden_layers=5, hidden_sizes=[150, 250, 400, 250, 150], epochs=100)
+        learning_rate=0.001, dropout=0.15, hidden_layers=5, hidden_sizes=[150, 250, 400, 250, 150], epochs=5)
     combinedNN.train_model(verbose=1)
     combinedNN.test_model(verbose=1, correct_only=0)
 
