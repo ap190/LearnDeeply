bd4b6ec0d011e3ff582ddd51cf4f21551b094818
Qi Feng <qi_feng@brown.edu>
2018-12-08 22:33:10 -0500

    Tried different parameters for metadata nn, now accuracy 1283/5985

--- Models/main.py
+++ Models/main.py
@@ -22,7 +22,7 @@ import combined_NN
 # ==================== METADATA
 def metadata():
     inputs, labels = data.metadata['inputs'], data.metadata['labels']
-    metaNN = metadata_NN.Model(inputs=inputs, labels=labels)
+    metaNN = metadata_NN.Model(inputs=inputs, labels=labels, learning_rate=0.001, dropout=0, hidden_layers=3, hidden_sizes=[256, 256, 256])
     metaNN.train_model(verbose=1)
     metaNN.test_model(verbose=1)
 
@@ -42,27 +42,29 @@ def imageclass():
 
 # ==================== MAIN.PY
 def main():
-    inputs, labels = data.metadata['inputs'], data.metadata['labels']
-    metaNN = metadata_NN.Model(inputs=inputs, labels=labels, 
-        learning_rate=0.001, dropout=0, hidden_layers=3, hidden_sizes=[100, 150, 100])
+    # inputs, labels = data.metadata['inputs'], data.metadata['labels']
+    # metaNN = metadata_NN.Model(inputs=inputs, labels=labels, 
+    #     learning_rate=0.001, dropout=0, hidden_layers=3, hidden_sizes=[100, 150, 100])
 
-    print(metaNN.model.get_weights())
+    # print(metaNN.model.get_weights())
 
-    meta_output = metaNN.get_output_layer()
+    # meta_output = metaNN.get_output_layer()
 
-    inputs, labels = data.image_class['inputs'], data.image_class['labels']
-    probabilities, vocab_size = data.image_class['probabilities'], data.image_class['vocab_size']
-    imageNN = imageclass_NN.Model(inputs=inputs, labels=labels, probabilities=probabilities, vocab_size=vocab_size,
-        learning_rate=0.001, embed_size=50, dropout=0, hidden_layers=3, hidden_sizes=[100, 150, 100])
+    # inputs, labels = data.image_class['inputs'], data.image_class['labels']
+    # probabilities, vocab_size = data.image_class['probabilities'], data.image_class['vocab_size']
+    # imageNN = imageclass_NN.Model(inputs=inputs, labels=labels, probabilities=probabilities, vocab_size=vocab_size,
+    #     learning_rate=0.001, embed_size=50, dropout=0, hidden_layers=3, hidden_sizes=[100, 150, 100])
 
-    image_output = imageNN.get_output_layer()
+    # image_output = imageNN.get_output_layer()
 
-    combined_inputs = keras.layers.concatenate([meta_output, image_output])
-    combined_labels = labels
-    combinedNN = combined_NN.Model(inputs=combined_inputs, labels=combined_labels, 
-        learning_rate=0.001, dropout=0.15, hidden_layers=5, hidden_sizes=[150, 250, 400, 250, 150], epochs=5)
-    combinedNN.train_model(verbose=1)
-    print(metaNN.model.get_weights())
+    # combined_inputs = keras.layers.concatenate([meta_output, image_output])
+    # combined_labels = labels
+    # combinedNN = combined_NN.Model(inputs=combined_inputs, labels=combined_labels, 
+    #     learning_rate=0.001, dropout=0.15, hidden_layers=5, hidden_sizes=[150, 250, 400, 250, 150], epochs=5)
+    # combinedNN.train_model(verbose=1)
+    # print(metaNN.model.get_weights())
+
+    metadata()
 
     # combinedNN.test_model(verbose=1, correct_only=0)
 
