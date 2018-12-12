1219146c294a23d32bb06b047747ffcc295ed48f
Qi Feng <qi_feng@brown.edu>
2018-12-11 23:15:32 -0500

    deleted some files

--- script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# A script to collect lots of data.
-python instagram-crawler/crawler.py -u selenagomez
-python instagram-crawler/crawler.py -u taylorswift
-python instagram-crawler/crawler.py -u leomessi
